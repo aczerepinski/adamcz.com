29303f82ca0b0f5225566948df5e96c29f7b6e90
aczerepinski <aczerepinski@gmail.com>
2016-01-19 08:49:25 -0500

    comment out force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :adamcz_dot_com, AdamczDotCom.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "http", host: "adamcz.com", port: 443],
-force_ssl: [rewrite_on: [:x_forwarded_proto]],
+# force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
